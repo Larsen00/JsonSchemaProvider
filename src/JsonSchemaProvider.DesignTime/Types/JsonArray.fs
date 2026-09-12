ee0e921a29d86ef409320a516a48333a84b325f8
Larsen <jonas.larsen103@gmail.com>
2026-09-12 13:29:09 +0200

    Refactor FSharpListType to use recursion for handling MinItems and improve keyword management

--- src/JsonSchemaProvider.DesignTime/Types/JsonArray.fs
+++ src/JsonSchemaProvider.DesignTime/Types/JsonArray.fs
@@ -4,12 +4,15 @@ module JsonArrayProvidedType =
     open System
     open JsonSchemaProvider
 
-    let FSharpListType (innerStaticType: Type) (arrayKeywords: JsonArray.Keywords) (compileFlags: ProviderConfiguration.CompileFlags) =
+    let rec FSharpListType (innerStaticType: Type) (arrayKeywords: JsonArray.Keywords) (compileFlags: ProviderConfiguration.CompileFlags) =
         match arrayKeywords.specific with
         | { MinItems = Some minItems } when compileFlags.CompileMinItems ->
 
+            let new_keywords = { arrayKeywords with specific = { arrayKeywords.specific with MinItems = None } }
+
+            let listType = FSharpListType innerStaticType new_keywords compileFlags
+
             // This generates a tuple where the if the minItems is n > 0 then the tuple will be T * T * ... * T * List<T> where T is the innerStaticType and there are n occurrences of T in the tuple.
-            let listType = typedefof<_ list>.MakeGenericType innerStaticType
             Array.append (Array.create minItems innerStaticType) [| listType |]
             |> Microsoft.FSharp.Reflection.FSharpType.MakeTupleType
 
